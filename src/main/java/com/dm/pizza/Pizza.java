9d4214e3954410a4f1a21586782eca53ef3aa3e5
Dorota Miler <dorota.miler@outlook.com>
2020-01-26 23:34:30 +0100

    methods modifications

--- src/main/java/com/dm/pizza/Pizza.java
+++ src/main/java/com/dm/pizza/Pizza.java
@@ -2,17 +2,19 @@ package com.dm.pizza;
 
 public class Pizza {
 
-    private double price;
-    private double diameter;
-    private double radius = diameter / 2;
-    private double size = Math.PI * Math.pow(radius, 2);
+     double smallPizzaPrice = 15;
+     double mediumPizzaPrice = 35;
+     double bigPizzaPrice = 50;
+     double diameter;
+     double radius = diameter / 2;
+     double size = Math.PI * Math.pow(radius, 2);
 
 
     public Pizza(double price, double size){
         pizzaRatio(price,size);
     }
     public void pizzaRatio(double price, double size) {
-        System.out.println("Pizza's price-size ratio equals: " + getPrice() / getSize());
+        System.out.println("Pizza's price-size ratio equals: " + getPrice() / size;
     }
 
     public double getDiameter() {
@@ -27,7 +29,4 @@ public class Pizza {
         return radius;
     }
 
-    public double getSize() {
-        return size;
-    }
 }
--- src/main/java/com/dm/pizza/PizzaApp.java
+++ src/main/java/com/dm/pizza/PizzaApp.java
@@ -1,6 +1,8 @@
 package com.dm.pizza;
 
 
+import java.util.Scanner;
+
 public class PizzaApp {
     public static void main(String[] args) {
 
@@ -13,12 +15,18 @@ public class PizzaApp {
         A może pizzę 30 cm za 20 zł?
 
         */
+    }
 
-        public double pizzasComparison {
-            Pizza pizza1 = new Pizza();
+    public double pizzasComparison{
+        System.out.println("Hi! Welcome to DM's pizza!" + "\nWhat pizza size would you like to order?" + "\n\n(advise of pizza diameter of choice: small -> 25cm, medium -> 40cm or big -> 50cm)" + "\n.......");
+        Scanner scan = new Scanner(System.in);
+        double chosenSize = scan.nextDouble();
+        System.out.println("Thank you. This pizza costs: "+);
 
-            Pizza pizza2 = new Pizza();
+        Pizza pizza1 = new Pizza();
 
-        }
+        Pizza pizza2 = new Pizza();
+
     }
 }
+
