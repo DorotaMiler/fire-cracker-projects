b1036280e39a60c53074d3f98e664aa54181c0dd
DorotaMiler <dorota.miler@outlook.com>
2020-02-15 19:26:19 +0100

    PizzaSize - parameters type change from PizzaDiameter to int in pizzaSize()

--- src/main/java/com/dm/pizza/PizzaSize.java
+++ src/main/java/com/dm/pizza/PizzaSize.java
@@ -4,14 +4,14 @@ public class PizzaSize {
 
     private double size;
 
-    public double PizzaSize(PizzaDiameter pizzaDiameter){
+    public double pizzaSize(int pizzaDiameter){
         int diameter = pizzaDiameter.getDiameter();
         double radius = (double)diameter/2;
         return size = Math.PI * Math.pow(radius, 2);
     }
-    
-    public void printSize(){
-        System.out.println(PizzaSize(PizzaDiameter pizzaDiameter));
+
+    public void printSize(int diameter){
+        System.out.println("Pizza's size is: "+pizzaSize(diameter);
     }
 
     public double getSize() {
